50333741429315b849ae2d20513b7ed2ffc418fc
gunanidhi-ms <gunanidhims2005@gmail.com>
2025-09-18 10:09:17 +0530

    Frontend Commit

---
+++ mern_stack/src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>MERN Stack Project</h1>
+      <p>Welcome to the MERN Stack application!</p>
+    </div>
+  );
+}
+
+export default App;
